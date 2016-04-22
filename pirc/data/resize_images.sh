c13f64223e1d9a0f1217d00d4d34303b69912b35
Andrew Sharp <asharp@utexas.edu>
2016-04-22 10:01:30 -0500

    adding finetuned network

--- pirc/data/resize_images.sh
+++ pirc/data/resize_images.sh
@@ -1,18 +1,18 @@
 EXAMPLE=.
 
-for file in $EXAMPLE/dr_ss/*
+for file in $EXAMPLE/dr_256/*
 do
 	if [ ! -d "$file" ]
 		then
 		echo $file
-		convert $file -resize 227x227! $file;
+		convert $file -resize 256x256! $file;
 	fi
 done
-for file in $EXAMPLE/dc_ss/*
+for file in $EXAMPLE/dc_256/*
 do
 	if [ ! -d "$file" ]
 		then
 		echo $file
-		convert $file -resize 227x227! $file;
+		convert $file -resize 256x256! $file;
 	fi
 done

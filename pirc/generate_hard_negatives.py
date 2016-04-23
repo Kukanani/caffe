0d5b8d19f4eb958ffd5becd5a7c1a6cf56ab9134
Adam Allevato <adam.d.allevato@gmail.com>
2016-04-23 09:51:15 -0500

    Updated hard negative mining for maverick testing

--- pirc/generate_hard_negatives.py
+++ pirc/generate_hard_negatives.py
@@ -9,12 +9,25 @@ import sys
 import csv
 import operator
 # parse command line arguments or use defaults
+
+ground_truth_csv_path = ''
+input_csv_path = ''
+
+output_ref_path = ''
+output_cq_path = ''
+
+all_pairs = []
+
 def parse_args():
 	if len(sys.argv) < 5:
-		print "Usage: calculate_map ground_truth_csv input_csv test1(reference) test2(consumer)"
-		return false
+		#print 'Usage: calculate_map ground_truth_csv input_csv test1(reference)_tomodify test2(consumer)_tomodify'
+		return False
 	else:
-		return (sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+		ground_truth_csv_path = sys.args[1]
+		input_csv_path = sys.args[2]
+		output_ref_path = sys.args[3]
+		output_cq_path = sys.args[4]
+		return True
 
 # loads a "dense" matrix (list of row/columns that should have 1s) and turn them into a dict of lists -- the reference image matches for each consumer image.
 # used for loading the ground truth tables
@@ -71,28 +84,42 @@ def find_hard_negatives(ground_truth, input_matrix):
 			while hard_negs_for_this_cq != NUM_HARD_NEGATIVES:
 				rank_tuple = ascending_rank_tuples[rank]
 				if rank_tuple[1] is not rank_1 and rank_tuple[1] is not rank_2:
-					hard_negatives.append([cq,rank_tuple[0]])
+					hard_negatives.append([rank_tuple[0],cq,0])
 					hard_negs_for_this_cq = hard_negs_for_this_cq + 1
 				rank = rank + 1
 		ctr = ctr+1
 
 	return hard_negatives
 
-def append_negative_pairs_to_files(cq_file, ref_file):
-	pass
+def save_pairs_to_files(all_pairs, ref_path, cq_path):
+	with open(ref_path, 'w') as ref_file, open(cq_path, 'w') as cq_file:
+		for i in len(all_pairs):
+			ref_file.write(all_pairs[0] + ' ' + str(all_pairs[2]) + '\n')
+			cq_file.write(all_pairs[1] + ' ' + str(all_pairs[2]) + '\n')
 
-def shuffle_files_linked(file1, file2):
-	pass
+def load_pairs_from_files(ref_path, cq_path):
+	pairs = []
+	with open(ref_path, 'r') as ref_file, open(cq_path, 'r') as cq_file:
+		ref_length = 0
+		cq_length = 0
+		for i, ref_line in enumerate(ref_file):
+			cq_line = cq_file.readline()
+			pairs.append([ref_line.split(' ')[0], cq_line.split(' ')[0], int(ref_line.split(' ')[1])])
+			ref_length = i
+		cq_length = sum(1 for cq_line in cq_file)
+		if cq_length != ref_length:
+			print 'Error while loading files ' + ref_path + ' and ' + cq_path + ': the files don\'t have the same number of lines!'
+	return pairs
 
 if __name__ == "__main__":
-	args = parse_args()
-
-	if args:
-		ground_truth = load_dense(args[0])
-		input_matrix = load_matrix(args[1])
+	if parse_args():
+		ground_truth = load_dense(ground_truth_csv_path)
+		input_matrix = load_matrix(input_csv_path)
 		hard_negatives = find_hard_negatives(ground_truth, input_matrix)
-		append_negative_pairs_to_files(args[2], args[3])
-		shuffle_files_linked(args[2], args[3])
+		all_pairs = load_pairs_from_files(output_ref_path, output_cq_path)
+		all_pairs.append(hard_negatives)
+		shuffle(all_pairs)
+		save_pairs_to_files(all_pairs, output_ref_path, output_cq_path)
 		print '============================'
 
 		print '{} hard negatives found.'.format(len(hard_negatives))

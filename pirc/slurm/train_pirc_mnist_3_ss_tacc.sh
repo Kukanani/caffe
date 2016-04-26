899edb6bcfb68c2ba84c2d65277ffd9233731a81
Adam Allevato <adam.d.allevato@gmail.com>
2016-04-26 11:05:50 -0500

    MNIST hard negative training

--- pirc/slurm/train_pirc_mnist_3_ss_tacc.sh
+++ pirc/slurm/train_pirc_mnist_3_ss_tacc.sh
@@ -3,4 +3,4 @@ TOOLS=/work/01932/dineshj/CS381V/caffe_install_scripts/caffe/build/tools
 BASE=/work/04002/aa67857/caffe/pirc
 
 cd $BASE
-$TOOLS/caffe train --solver=$BASE/pirc_mnist_3_ss_solver.prototxt --snapshot=$BASE/mnist_3_ss/mnist_3_ss_iter_5000.solverstate
+$TOOLS/caffe train --solver=$BASE/pirc_mnist_3_ss_solver.prototxt
